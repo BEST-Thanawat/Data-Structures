86ab3e499ac35c56b7dcc5c1dc045b8e192df40b
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-08-17 15:01:12 -0700

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Array
+            int[] arrayInt = new int[] {1, 2, 3, 4, 5 };
+
+            //Declare Array Double
+            double[] arrayDouble = new double[2] { 1000.11, 2000.22 }; 
+
+            //Declare Array String
+            string[] arrayString = new string[2];
+            arrayString[0] = "item1";
+            arrayString[1] = "item2";
+
+            //Declare Array Float
+            float[] arrayFloat;
+            arrayFloat = new float[] { 0.1f, 0.25f, 0.5f, 0.75f, 1f };
+
+            //Declare Array Char
+            char[] arrayChar = new char[] { 'a', 'b', 'c'};
+            char[] getArrayChar = arrayChar;
+
+            //Declare Array Object
+            Node[] arrayNode = new Node[3];
+            arrayNode[0] = new Node(1);
+            arrayNode[1] = new Node(2);
+            arrayNode[2] = new Node(3);
+            arrayNode[0].Left = arrayNode[1];
+            arrayNode[0].Right = arrayNode[2];
+
+            //Declare Jagged Array Int
+            int[][] jaggedArrayInt = new int[2][]
+            {
+                new int[] { 1, 2 },
+                new int[] { 1, 2, 3, 4 }
+            };
+
+            //Declare Jagged Array String
+            string[][] jaggedArrayString = new string[][]
+            {
+                new string[] { "cat", "rat" },
+                new string[] { "cat", "rat", "bat" },
+                new string[] { "cat", "rat", "bat", "fat" }
+            };
+
+
+            //Display item in array
+            for (int i = 0; i < arrayChar.Length; i++)
+            {
+                Console.Write(arrayChar[i] + ",");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (var item in jaggedArrayString)
+            {
+                foreach (var item2 in item)
+                {
+                    Console.Write(item2 + ",");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Node
+    {
+        public int Data;
+        public Node Left;
+        public Node Right;
+
+        public Node(int data)
+        {
+            Data = data;
+        }
+    }
+}
---
+++ Linked List/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Linked List
+            //Add 1, 2, 3, 4, 5
+            LinkedList<int> linkedListInt = new LinkedList<int>();
+            linkedListInt.AddLast(1);
+            linkedListInt.AddLast(5);
+            linkedListInt.AddAfter(linkedListInt.First, 2);
+            linkedListInt.AddAfter(linkedListInt.Find(2), 3);
+            linkedListInt.AddBefore(linkedListInt.Last, 4);
+
+            //Remove 3
+            linkedListInt.Remove(3);
+
+            //Remove first and last
+            linkedListInt.RemoveFirst();
+            linkedListInt.RemoveLast();
+
+            foreach (var item in linkedListInt)
+            {
+                Console.Write(item + ",");
+            }
+            Console.WriteLine();
+
+            if (linkedListInt.Contains(2))
+            {
+                Console.Write("Element found !!!");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Declare Linked List Object
+            LinkedList<Node> linkedListNode = new LinkedList<Node>();
+            LinkedListNode<Node> nodeToAdd1 = new LinkedListNode<Node>(new Node(1));
+            linkedListNode.AddLast(nodeToAdd1);
+            Node node2 = new Node(2);
+            linkedListNode.AddLast(node2);
+            linkedListNode.Find(node2).Value.Left = new Node(3);
+            LinkedListNode<Node> result = linkedListNode.Find(node2);
+            Console.WriteLine("Found element " + result.Value.Data + " in LinkedList");
+            Console.WriteLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Node
+    {
+        public int Data;
+        public Node Left;
+        public Node Right;
+        public Node(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+}
