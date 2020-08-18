1232c7318335bcb998f8958c7e458440fd78f6a6
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-08-18 12:17:53 -0700

    Add Stack and Queue

---
+++ Queue/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Queue_DT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Queue FIFO
+            //Enqueue Dequeue Peek Clear
+            //Generic (Same type)
+            //Non-Generic (Different type)
+
+            //Declare Non-Generic Queue
+            Queue queueNonGeneric = new Queue();
+            queueNonGeneric.Enqueue(1);
+            queueNonGeneric.Enqueue("2");
+            queueNonGeneric.Enqueue(3.0f);
+            Console.WriteLine("This is non-generic Queue");
+
+            Console.Write("Contains ");
+            foreach (var item in queueNonGeneric)
+            {
+                Console.Write(item + ",");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Dequeue 1 time");
+            queueNonGeneric.Dequeue();
+            Console.Write("Right now the queue contains ");
+            foreach (var item in queueNonGeneric)
+            {
+                Console.Write(item + ",");
+            }
+            Console.WriteLine();
+            Console.WriteLine("The first item in the queue is " + queueNonGeneric.Peek());
+
+            Console.WriteLine();
+            Console.WriteLine("Declare a generic Queue by Enqueue 3 characters");
+            Queue<char> queueGeneric = new Queue<char>();
+            queueGeneric.Enqueue('a');
+            queueGeneric.Enqueue('b');
+            queueGeneric.Enqueue('c');
+            Console.Write("Right now the generic queue contains ");
+            foreach (var item in queueGeneric)
+            {
+                Console.Write(item + ",");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Stack_DT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack LIFO
+            //Push Peek Pop Clear
+            //Generic (Same type)
+            //Non-Generic (Different type)
+
+            //Delcare non-generic Stack
+            Stack stack = new Stack();
+
+            //Insert 1, 2, 3 into stack
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            //Return top value of stack
+            Console.WriteLine("This is top value of stack: " + stack.Peek());
+            Console.WriteLine();
+
+            //Remove top value of stack
+            stack.Pop();
+            Console.WriteLine("Pop the stack");
+            Console.WriteLine("This is top value of stack: " + stack.Peek());
+            Console.WriteLine();
+
+            Console.WriteLine("Now stack contains " + stack.Count + " elements");
+            Console.WriteLine();
+
+            stack.Push("C#");
+            stack.Push("C++");
+            Console.WriteLine("Add 2 more items into stack");
+            Console.WriteLine("This is top value of stack: " + stack.Peek());
+            Console.WriteLine();
+
+            Console.WriteLine("Below are items inside stack");
+            foreach (var item in stack)
+            {
+                Console.Write(item + ",");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Push a Node into stack");
+            stack.Push(new Node(100));
+            Console.WriteLine("This is top value of stack: " + ((Node)stack.Peek()).Data);
+
+            //Declare generic stack
+            Stack<int> genericStackInt = new Stack<int>();
+            genericStackInt.Push(1);
+            genericStackInt.Push(2);
+            genericStackInt.Push(3);
+            Console.WriteLine();
+
+            Console.WriteLine("This is generic Stack and must be stored same type value");
+            foreach (var item in genericStackInt)
+            {
+                Console.Write(item + ",");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Node
+    {
+        public int Data;
+        public Node Left;
+        public Node Right;
+        public Node(int data)
+        {
+            Data = data;
+        }
+    }
+}
