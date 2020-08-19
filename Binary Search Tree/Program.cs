1d073fce1ccc02f04e2d324d1539d1a6b84f4ed9
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-08-18 20:36:56 -0700

    Add DFS.

---
+++ Binary Search Tree/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Transactions;
+
+namespace Binary_Search_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = new Node(10);
+            root.Left = new Node(5);
+            root.Right = new Node(15);
+
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.Root = root;
+
+            bst.Add(1);
+
+            foreach (var item in bst.DFS_PostOrder())
+            {
+                Console.WriteLine(item + ",");
+            }
+        }
+    }
+
+    public class Node
+    {
+        public int Value;
+        public Node Left;
+        public Node Right;
+
+        public Node(int value)
+        {
+            Value = value;
+            Right = null;
+            Left = null;
+        }
+    }
+
+    public class BinarySearchTree
+    {
+        public Node Root { get; set; }
+        public BinarySearchTree()
+        {
+            Root = null;
+        }
+        public bool Add(int value)
+        {
+            Node newNode = new Node(value);
+
+            if (Root == null)
+            {
+                Root = newNode;
+                return true;
+            }
+
+            bool traversing = true;
+            Node currentNode = Root;
+            while (traversing)
+            {
+                if (newNode.Value < currentNode.Value)
+                {
+                    if (currentNode.Left != null)
+                    {
+                        currentNode = currentNode.Left;
+                    }
+                    else
+                    {
+                        currentNode.Left = newNode;
+                        traversing = false;
+                    }
+                }
+                else if (newNode.Value > currentNode.Value)
+                {
+                    if (currentNode.Right != null)
+                    {
+                        currentNode = currentNode.Right;
+                    }
+                    else
+                    {
+                        currentNode.Right = newNode;
+                        traversing = false;
+                    }
+                }
+                else
+                {
+                    traversing = false;
+                }
+            }
+
+            return true;
+        }
+
+        public List<int> BFS()
+        {
+            if (Root == null) return null;
+
+            List<int> result = new List<int>();
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(Root);
+
+            while (queue.Count > 0)
+            {
+                if (queue.Peek().Left != null)
+                {
+                    queue.Enqueue(queue.Peek().Left);
+                }
+
+                if (queue.Peek().Right != null)
+                {
+                    queue.Enqueue(queue.Peek().Right);
+                }
+                result.Add(queue.Peek().Value);
+                queue.Dequeue();
+            }
+            return result;
+        }
+
+        public List<int> DFS_InOrder()
+        {
+            if (Root == null) return null;
+
+            List<int> result = new List<int>();
+            Stack<Node> stack = new Stack<Node>();
+            Node currentNode = Root;
+
+            while (stack.Count > 0 || currentNode != null)
+            {
+                if (currentNode != null)
+                {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.Left;
+                }
+                else
+                {
+                    currentNode = stack.Pop();
+                    result.Add(currentNode.Value);
+
+                    currentNode = currentNode.Right;
+                }
+            }
+
+            return result;
+        }
+
+        public List<int> DFS_PreOrder()
+        {
+            if (Root == null) return null;
+
+            List<int> result = new List<int>();
+            Stack<Node> stack = new Stack<Node>();
+            Node currentNode = null;
+            stack.Push(Root);
+
+            while(stack.Count > 0)
+            {
+                currentNode = stack.Pop(); 
+                result.Add(currentNode.Value);
+
+                if (currentNode.Right != null)
+                {
+                    stack.Push(currentNode.Right);
+                }
+                if (currentNode.Left != null)
+                {
+                    stack.Push(currentNode.Left);
+                }
+            }
+
+            return result;
+        }
+
+        public List<int> DFS_PostOrder()
+        {
+            if (Root == null) return null;
+
+            Stack<Node> visited = new Stack<Node>();
+            Stack<Node> stack = new Stack<Node>();
+            visited.Push(Root);
+
+            while (visited.Count > 0)
+            {
+                Node currentNode = visited.Pop();
+                stack.Push(currentNode);
+
+                if(currentNode.Left != null)
+                {
+                    visited.Push(currentNode.Left);
+                }
+
+                if (currentNode.Right != null)
+                {
+                    visited.Push(currentNode.Right);
+                }
+            }
+
+            return stack.ToList<Node>().Select(x => x.Value).ToList(); ;
+        }
+    }
+}
