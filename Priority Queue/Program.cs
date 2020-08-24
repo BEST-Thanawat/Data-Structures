93e627420f09360e2f20bc07c8fa2d2e26afbe6b
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-08-23 22:28:50 -0700

    Add priority queue.

---
+++ Priority Queue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Priority_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Node
+    {
+        public string Value { get; set; }
+        public int Priority { get; set; }
+
+        public Node(string value, int priority)
+        {
+            Value = value;
+            Priority = priority;
+        }
+    }
+}
