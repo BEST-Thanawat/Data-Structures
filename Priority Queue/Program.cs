8868c2e89c7233bd92e0cb035010bbf9281b12e3
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2020-09-22 09:39:52 -0700

    Add  BubbleUp().

--- Priority Queue/Program.cs
+++ Priority Queue/Program.cs
@@ -12,6 +12,8 @@ namespace Priority_Queue
             PriorityQueue priorityQueue = new PriorityQueue();
             priorityQueue.Values.Add(new PriorityQueue.Node("ABC", 9));
             priorityQueue.Values.Add(new PriorityQueue.Node("DEF", 1));
+            priorityQueue.Values.Add(new PriorityQueue.Node("GHI", 100));
+            priorityQueue.Values.Add(new PriorityQueue.Node("JKL", 235));
         }
     }
 
@@ -35,6 +37,36 @@ namespace Priority_Queue
             }
         }
 
+        public bool Enqueue(string value, int priority)
+        {
+            if (Values.Count == 0)
+            {
+                Values.Add(new Node(value, priority));
+                return true;
+            }
+
+            Values.Add(new Node(value, priority));
+
+            return BubbleUp();
+        }
+
+        private bool BubbleUp()
+        {
+            int elementIndex = Values.Count - 1;
+            Node nodeToBubbleUp = Values[elementIndex];
+            int parentIndex = (elementIndex - 1) / 2;
+
+            while (parentIndex >= 0 && nodeToBubbleUp.Priority > Values[parentIndex].Priority)
+            {
+                Values[elementIndex] = Values[parentIndex];
+                Values[parentIndex] = nodeToBubbleUp;
+
+                elementIndex = parentIndex;
+                parentIndex = (elementIndex - 1) / 2;
+            }
+
+            return true;
+        }
     }
     
 }
